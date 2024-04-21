66c79b8cb35ff516b373f0c455e2609077c5b40e
AnnaRossova <rossova.anna@gmail.com>
2024-04-21 15:36:27 +0300

    img on click change

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,8 +1,22 @@
-const ImageCard = ({ image, openModal }) => {
+/* const ImageCard = ({ image, openModal }) => {
     return (
       <div onClick={() => openModal(image)}>
         <img src={image.urls.small} alt={image.alt_description} />
       </div>
+    );
+  };
+  
+  export default ImageCard; */
+
+  const ImageCard = ({ image, openModal }) => {
+    return (
+      <div><img
+      src={image.urls.small}
+      alt={image.alt_description}
+      onClick={() => openModal(image)}
+    />
+    </div>
+      
     );
   };
   
